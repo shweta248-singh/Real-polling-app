656dfbd305d5328747bc52d547cf63cebb3519b9
shweta248-singh <shwetashweta96436@gmail.com>
2026-04-17 00:06:49 +0530

    change bell icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
           {/* 🔥 Right Section */}
           <div className="flex items-center gap-4">
 
-            {/* 🔔 Notification Icon (future use) */}
+            
             {/* <button className="relative p-2 rounded-full hover:bg-slate-100 transition">
               <Bell className="w-5 h-5 text-slate-500" />
               <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
